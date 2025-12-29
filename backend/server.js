757174f47493d755c5f0c773d9883ae6d0033f8d
Arunav Singh <arunavsingh999@gmail.com>
2025-12-30 02:43:12 +0530

    configured mongo and server
    server started successfully and mongo connection was estabalished successfully

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    system: "PurpleMerit Backend Online",
+  });
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("DB connection error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
