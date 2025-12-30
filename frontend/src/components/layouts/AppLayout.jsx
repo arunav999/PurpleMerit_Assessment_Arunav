a0be24ebea511fb3702ef6afb596886f53849dd7
Arunav Singh <arunavsingh999@gmail.com>
2025-12-31 03:33:12 +0530

    app layout added and contex added in main and router added in main

--- frontend/src/components/layouts/AppLayout.jsx
+++ frontend/src/components/layouts/AppLayout.jsx
@@ -1,8 +1,24 @@
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const AppLayout = () => {
   return (
-    <>
-      <div className="">App Layout</div>
-    </>
+    <div className="min-h-screen bg-gray-50 text-gray-900 font-sans antialiased">
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
+      <Outlet />
+    </div>
   );
 };
 
