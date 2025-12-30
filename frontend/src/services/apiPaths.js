e945a10e60efd339dda6d4704dd2289fd863b189
Arunav Singh <arunavsingh999@gmail.com>
2025-12-31 01:51:16 +0530

    axios instance created

--- frontend/src/services/apiPaths.js
+++ frontend/src/services/apiPaths.js
@@ -1,1 +1,6 @@
 // ==================== API BASE URL ====================
+export const BASE_URL = import.meta.env.VITE_PURPLEMERIT_BACKEND_URL;
+
+// ==================== API PATH CONSTANTS ====================
+// Common API prefix for all endpoints
+const API_COMMON = "/api/v1";
--- frontend/src/services/axiosClient.js
+++ frontend/src/services/axiosClient.js
@@ -1,2 +1,27 @@
 // ==================== AXIOS INSTANCE CONFIGURATION ====================
 import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+// Re-usable axios instance
+const axiosClient = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  withCredentials: true,
+});
+
+axiosClient.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      //
+    }
+
+    return Promise.reject(err);
+  }
+);
+
+export default axiosClient;
