2b8ec2e22dfbe2c6aa56c8fb906999f65d260ebe
Arunav Singh <arunavsingh999@gmail.com>
2025-12-31 03:02:03 +0530

    router setup

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,1 +1,27 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
+
+import AppLayout from "./components/layouts/AppLayout";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Dashboard from "./pages/Auth/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { index: true, element: <Navigate to="/dashboard" replace /> },
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <Signup /> },
+
+      // Protected routes
+      {
+        element: <ProtectedRoute />,
+        children: [{ path: "dashboard", element: <Dashboard /> }],
+      },
+    ],
+  },
+]);
+
+export default router;
