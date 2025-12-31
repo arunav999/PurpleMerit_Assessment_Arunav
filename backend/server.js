5309953774f02ecbdc76407e751bfc3a94c5b899
Arunav Singh <arunavsingh999@gmail.com>
2025-12-31 05:44:49 +0530

    made changes to server.js for vercel to work

--- backend/server.js
+++ backend/server.js
@@ -74,4 +74,12 @@ app.use(errorHandler);
 // ==================== Start Server ====================
 // Start listening for incoming requests
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+  });
+}
+
+module.exports = app;
