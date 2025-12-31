6d877f1f4a00583e47cfce07f01447821c81db35
Arunav Singh <arunavsingh999@gmail.com>
2025-12-31 07:14:04 +0530

    vercel ipV4 force use mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,36 @@
+// const mongoose = require("mongoose");
+
+// async function PurpleMeritDB() {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("PurpleMerit DB Connected");
+//   } catch (err) {
+//     console.log("Error connecting PurpleMeritDb:", err);
+//     process.exit(1);
+//   }
+// }
+
+// module.exports = PurpleMeritDB;
+
 const mongoose = require("mongoose");
 
-async function PurpleMeritDB() {
+const PurpleMeritDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    if (mongoose.connection.readyState >= 1) {
+      console.log("Using existing DB connection");
+      return;
+    }
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      family: 4,
+    });
+
     console.log("PurpleMerit DB Connected");
-  } catch (err) {
-    console.log("Error connecting PurpleMeritDb:", err);
-    process.exit(1);
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+
+    throw new Error("DB Connection Failed");
   }
-}
+};
 
 module.exports = PurpleMeritDB;
